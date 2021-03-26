8b5e7e632251f9cf1f26faa749220085e728c7e8
Anibal Portillo <48638366+AnibalLP@users.noreply.github.com>
2021-03-26 00:06:00 -0600

    iniciando el crud

--- crudlaravel/webpack.mix.js
+++ crudlaravel/webpack.mix.js
@@ -11,8 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.scripts(['resources/js/vue.js',
-        'resources/js/axios.js',
-        'resources/js/app.js'
-    ],
-    'public/js/app.js');
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        //
+    ]);
